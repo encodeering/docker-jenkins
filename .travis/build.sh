7f8c10af0a585ac9533203b037d4da3bb998e215
Michael Clausen <encodeering@gmail.com>
2016-05-15 19:21:04 +0200

    integrates the dind secondary image for all platforms
    * starting with a semantic versioning, initial 0.1.0

--- .travis/build.sh
+++ .travis/build.sh
@@ -5,14 +5,25 @@ set -ev
 SEMANTIC="${VERSION:0:3}"
 
 TAG="$REPOSITORY/$PROJECT-$ARCH"
-TAGSPECIFIER="$SEMANTIC"
+TAGSPECIFIER="$SEMANTIC${CUSTOM:+-$CUSTOM}"
 
 docker pull   "$REPOSITORY/openjdk-$ARCH:8-jdk"
 docker tag -f "$REPOSITORY/openjdk-$ARCH:8-jdk" "java:8-jdk"
 
-docker build -t "$TAG:$TAGSPECIFIER" \
-             --build-arg JENKINS_VERSION="$VERSION" \
-             --build-arg JENKINS_SHA="$SHA"         \
-             "$PROJECT"
+case "$CUSTOM" in
+    walle )
+        docker build -t "$TAG:$TAGSPECIFIER"      \
+                     --build-arg SCRIPT="$SCRIPT" \
+                     "contrib/$CUSTOM"
 
-docker run --rm "$TAG:$TAGSPECIFIER" java -jar /usr/share/jenkins/jenkins.war --version
+        docker run --rm "$TAG:$TAGSPECIFIER" -v
+        ;;
+    * )
+        docker build -t "$TAG:$TAGSPECIFIER" \
+                     --build-arg JENKINS_VERSION="$VERSION" \
+                     --build-arg JENKINS_SHA="$SHA"         \
+                     "$PROJECT"
+
+        docker run --rm "$TAG:$TAGSPECIFIER" java -jar /usr/share/jenkins/jenkins.war --version
+        ;;
+esac
