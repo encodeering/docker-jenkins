7fc6130a0a7053c2f922906390991715bce3dea7
Michael Clausen <encodeering@gmail.com>
2016-05-17 19:29:57 +0200

    adds a compilation step to build tini for the target architecture
    * installing the required compile-time dependencies, as we only want to build tini without any subsequent verification steps (#building-tini)
    ** custom ci script requires an arm configuration for the package management
    
    NOTE
    
    * using v0.9.0 instead of the original v0.5.0 version

--- .travis/build.sh
+++ .travis/build.sh
@@ -6,6 +6,27 @@ SEMANTIC="${VERSION:0:3}"
 
 TAG="$REPOSITORY/$PROJECT-$ARCH"
 TAGSPECIFIER="$SEMANTIC${CUSTOM:+-$CUSTOM}"
+TINIBUILDPACK="$REPOSITORY/buildpack-$ARCH:jessie"
+TINIDIRECTORY=mktini
+TINIVERSION="v0.9.0"
+
+mktini () {
+    rm -rf   "$TINIDIRECTORY"
+    mkdir -p "$TINIDIRECTORY"
+
+    cat <<-EOF > "$TINIDIRECTORY/Dockerfile.tini.static"
+		FROM $TINIBUILDPACK
+		ENV CFLAGS="-DPR_SET_CHILD_SUBREAPER=36 -DPR_GET_CHILD_SUBREAPER=37" CC="gcc"
+		RUN apt-get update && apt-get -y install cmake git libcap-dev python-dev python-pip
+		RUN git clone --depth 1 --branch "$TINIVERSION" https://github.com/krallin/tini.git /usr/src/tini
+		WORKDIR /usr/src/tini
+EOF
+
+    docker build -f   "$TINIDIRECTORY/Dockerfile.tini.static" -t tini:static "$TINIDIRECTORY"
+    docker rm         "tini" || true
+    docker run --name "tini" tini:static bash -c "cmake . && make"
+    docker cp         "tini:/usr/src/tini/tini-static" "$PROJECT/tini"
+}
 
 docker pull   "$REPOSITORY/openjdk-$ARCH:8-jdk"
 docker tag -f "$REPOSITORY/openjdk-$ARCH:8-jdk" "java:8-jdk"
@@ -19,6 +40,8 @@ case "$CUSTOM" in
         docker run --rm "$TAG:$TAGSPECIFIER" -v
         ;;
     * )
+        mktini
+
         docker build -t "$TAG:$TAGSPECIFIER" \
                      --build-arg JENKINS_VERSION="$VERSION" \
                      --build-arg JENKINS_SHA="$SHA"         \
