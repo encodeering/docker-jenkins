52d0c9a74d7f430e7bd4c9e29688c756f238d9f9
Michael Clausen <encodeering@gmail.com>
2018-01-02 20:38:22 +0100

    updates to latest master version
    * tiny provides builds for armhf architectures now - patch can be deleted
    * tini is located under sbin
    
    NOTE
    
    * docker package is now named docker-ce and available for armhf as well
    * sha must be a sha256 computed value

--- .travis/build.sh
+++ .travis/build.sh
@@ -6,32 +6,11 @@ SEMANTIC="${VERSION:0:3}"
 
 TAG="$REPOSITORY/$PROJECT-$ARCH"
 TAGSPECIFIER="$SEMANTIC${CUSTOM:+-$CUSTOM}"
-TINIBUILDPACK="$REPOSITORY/buildpack-$ARCH:jessie"
-TINIDIRECTORY=mktini
-TINIVERSION="v0.9.0"
-
-mktini () {
-    rm -rf   "$TINIDIRECTORY"
-    mkdir -p "$TINIDIRECTORY"
-
-    cat <<-EOF > "$TINIDIRECTORY/Dockerfile.tini.static"
-		FROM $TINIBUILDPACK
-		ENV CFLAGS="-DPR_SET_CHILD_SUBREAPER=36 -DPR_GET_CHILD_SUBREAPER=37" CC="gcc"
-		RUN apt-get update && apt-get -y install cmake git libcap-dev python-dev python-pip
-		RUN git clone --depth 1 --branch "$TINIVERSION" https://github.com/krallin/tini.git /usr/src/tini
-		WORKDIR /usr/src/tini
-EOF
-
-    docker build -f   "$TINIDIRECTORY/Dockerfile.tini.static" -t tini:static "$TINIDIRECTORY"
-    docker rm         "tini" || true
-    docker run --name "tini" tini:static bash -c "cmake . && make"
-    docker cp         "tini:/usr/src/tini/tini-static" "$PROJECT/tini"
-}
 
 [[ "$JAVA" =~ ^8-jdk.* ]];
 
-docker pull   "$REPOSITORY/java-$ARCH:$JAVA"
-docker tag -f "$REPOSITORY/java-$ARCH:$JAVA" "java:8-jdk"
+docker pull "$REPOSITORY/java-$ARCH:$JAVA"
+docker tag  "$REPOSITORY/java-$ARCH:$JAVA" "openjdk:8-jdk"
 
 case "$CUSTOM" in
     walle* )
@@ -51,11 +30,7 @@ case "$CUSTOM" in
 
         ;;
     * )
-        if [[ "$JAVA" =~ .*-oracle$ ]]; then sed -i -r '/ENTRYPOINT/ s!/bin/tini!docker-eula-java", "/bin/tini!g' "$PROJECT/Dockerfile"; fi
-
-        patch -p1 --no-backup-if-mismatch --directory=$PROJECT < .patch/Dockerfile.patch
-
-        mktini
+        if [[ "$JAVA" =~ .*-oracle$ ]]; then sed -i -r '/ENTRYPOINT/ s!/sbin/tini!docker-eula-java", "/sbin/tini!g' "$PROJECT/Dockerfile"; fi
 
         docker build -t "$TAG:$TAGSPECIFIER" \
                      --build-arg JENKINS_VERSION="$VERSION" \
